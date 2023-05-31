9b766ee9a270fe3f5cfe5458c5900d51fb0fc9bd
Dokoye98 <dwayneokoye98@gmail.com>
2023-05-31 14:28:58 +0100

    Aggregation complete

--- Runner/Program.cs
+++ Runner/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Shop;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Items i1 = new Items(1, "Shirt", 'L',true);
+        Items i2= new Items(2, "Shorts", 'L', true);
+        Items i3 = new Items(3, "Forks", 'S', false);
+        Items[] itemlist =  new Items[] { i1, i2, i3 };
+        Trolley t1 = new Trolley(itemlist);
+
+        string[] names = t1.FullList();
+
+        for(int i = 0; i < names.Length; i++)
+        {
+            int a = i+1;
+            Console.WriteLine("This is item {0}:",a);
+            Console.WriteLine(names[i]);
+        }
+    }
+}
